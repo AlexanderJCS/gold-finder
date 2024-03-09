ad58667121ace42a8dcc9219b8408e8e3cc67c9f
Alex <alexandercastronovo@gmail.com>
2024-03-09 18:31:56 -0500

    add a CLI

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import argparse
+
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -5,6 +7,35 @@ from src.gold_finder import data_loading as dl
 from src.gold_finder import gold_finder as gf
 from src.helper import masking
 from src.network import density
+
+
+def get_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        prog="Golden",
+        description="Find gold particles and their density in electron microscopy images"
+    )
+    
+    parser.add_argument(
+        "name",
+        type=str,
+        help="The name of the dataset to analyze. This is the name of the folder in the 'analyzed synapses' directory,"
+             "e.g., 'S1' or 'S7'"
+    )
+    
+    parser.add_argument(
+        "-m", "--mask",
+        action="store_true",
+        help="Whether to apply the mask to the image before finding gold particles. Does not apply the mask if it "
+             "cannot find it. Default: False"
+    )
+    
+    parser.add_argument(
+        "-v", "--visual",
+        action="store_true",
+        help="Whether to display the image with the gold particles marked on it. Default: False"
+    )
+    
+    return parser.parse_args()
 
 
 def show_points_on_image(image, points):
@@ -22,21 +53,25 @@ def show_points_on_image(image, points):
 
 
 def main():
-    for bundle in dl.get_image_bundles("../data/analyzed synapses/"):
-        if bundle.name != "S4":
-            continue
+    args = get_args()
+    
+    bundles = dl.get_image_bundles("./data/analyzed synapses/")
+    bundle_dict = {bundle.name: bundle for bundle in bundles}
+    
+    if args.name not in bundle_dict:
+        print(f"Dataset '{args.name}' not found")
+        return
         
-        apply_mask = True
-        masked = masking.apply_mask(bundle.image, bundle.mask) if apply_mask else bundle.image
-        
-        gold_locations = gf.GoldFinder(masked).find_gold()
-        density_score = density.density(gold_locations)
-        
-        print(f"{density_score=}")
-        
-        show_points_on_image(masked, gold_locations)
-        
-        bundle.image.close()
+    bundle = bundle_dict[args.name]
+    image = masking.apply_mask(bundle.image, bundle.mask) if args.mask else bundle.image
+    
+    gold_locations = gf.GoldFinder(image).find_gold()
+    density_score = density.density(gold_locations)
+    
+    print(f"{density_score=}")
+    
+    if args.visual is True:
+        show_points_on_image(image, gold_locations)
 
 
 if __name__ == "__main__":
