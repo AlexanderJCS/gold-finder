ad8d4477ad976f9d1ec4c5eeb93fe6c6ea2f3c8d
Alex <alexandercastronovo@gmail.com>
2024-03-09 22:15:59 -0500

    Modify the output figure

--- src/helper/output/out.py
+++ src/helper/output/out.py
@@ -1,9 +1,12 @@
+import random
+
 from PIL import Image
 
 import numpy as np
 import pandas as pd
 
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 
 from src.network import density
 
@@ -23,26 +26,32 @@ def gen_visualization(image: Image, clusters: dict, display: bool, save_to: str)
     
     plt.imshow(np.array(image), cmap="gray")
     
+    legend = []
+    
     for cluster_num, cluster_values in clusters.items():
-        cluster_color = np.random.rand(3, )
+        cluster_color = (random.random(), random.random(), random.random())
+        
+        legend.append(mpatches.Patch(color=cluster_color, label=f"Cluster {cluster_num}"))
         
         plt.scatter(
             x=[coord[0] for coord in cluster_values],
             y=[coord[1] for coord in cluster_values],
             label=f"Cluster {cluster_num}",
-            alpha=0.9,
+            alpha=0.6,
             color=cluster_color,
-            s=7
+            s=3
         )
         
         for coord in cluster_values:
-            plt.text(coord[0], coord[1], f"C: {cluster_num}", fontsize=7, color="white")
+            plt.text(coord[0], coord[1], f"C: {cluster_num}", fontsize=5, color="white")
+    
+    plt.legend(handles=legend, prop={"size": 6})
     
     if display is True:
         plt.show()
     
     if save_to is not None:
-        plt.savefig(save_to)
+        plt.savefig(save_to, dpi=512)  # dpi = 512 to create an image that is sufficiently large
 
 
 def create_output_df(clusters: dict) -> pd.DataFrame:
